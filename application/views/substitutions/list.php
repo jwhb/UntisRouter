30f7829a453a89824ccb69c338a708cb62aec420
JWhy <jwhy@jwhy.de>
2013-12-16 20:52:00 +0100

    Add date navigator to grade overview, add day name
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/views/substitutions/list.php
+++ application/views/substitutions/list.php
@@ -1,24 +1,31 @@
-<table class="pure-table pure-table-bordered vp_table" >
-  <thead>
-    <tr>
-      <th>Klasse</th>
-      <th>Stunde</th>
-      <th>Fach</th>
-      <th>Raum</th>
-      <th>Art</th>
-      <th>Lehrer</th>
-    </tr>
-  </thead>
-  <tbody>
-<?php foreach($substitutions as $subst): ?> 
-    <tr>
-      <td><?php echo $subst->grade; ?></td>
-      <td><?php echo $subst->time; ?></td>
-      <td><?php echo $subst->class; ?></td>
-      <td><?php echo $subst->room; ?></td>
-      <td><?php echo $subst->type; ?></td>
-      <td><?php echo $subst->teacher; ?></td>
-    </tr>
-<?php endforeach; ?>
-    </tbody>
-</table>
+        <table class="pure-table pure-table-bordered vp_table" >
+          <thead>
+            <tr>
+              <th>Klasse</th>
+              <th>Stunde</th>
+              <th>Fach</th>
+              <th>Raum</th>
+              <th>Art</th>
+              <th>Lehrer</th>
+            </tr>
+          </thead>
+          <tbody>
+        <?php foreach($substitutions as $subst): ?> 
+            <tr>
+              <td><?php echo $subst->grade; ?></td>
+              <td><?php echo $subst->time; ?></td>
+              <td><?php echo $subst->class; ?></td>
+              <td><?php echo $subst->room; ?></td>
+              <td><?php echo $subst->type; ?></td>
+              <td><?php echo $subst->teacher; ?></td>
+            </tr>
+        <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        
+        <ul class="pure-paginator date_nav">
+            <li><a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['before']->format('d/m/Y')); ?>">&#171; <?php echo $dates['before']->format('D d.m.Y'); ?></a></li>
+            <li><a class="pure-button pure-button-active" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['base']->format('d/m/Y')); ?>"><?php echo $dates['base']->format('D d.m.Y'); ?></a></li>
+            <li><a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['after']->format('d/m/Y')); ?>"><?php echo $dates['after']->format('D d.m.Y'); ?> &#187;</a></li>
+        </ul>
