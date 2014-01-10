9c366db1dda2efa47b0b19e29334370a60058c5b
JWhy <jwhy@jwhy.de>
2014-01-10 21:54:32 +0100

    Add date nav to not found page
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/views/substitutions/not_found.php
+++ application/views/substitutions/not_found.php
@@ -1,2 +1,8 @@
-<p style="text-align: center;">Für die gew&uuml;nschte Stufe (<?php echo $grade; ?>) konnten keine Eintr&auml;ge am gew&uuml;nschten Datum (<?php echo $dates['base']->format('d.m.Y'); ?>) gefunden werden!
-<a href="<?php echo $this->config->base_url(); ?>"><br />Zur&uuml;ck</a></p>
+        <p style="text-align: center;">Für die gew&uuml;nschte Stufe (<?php echo $grade; ?>) konnten keine Eintr&auml;ge am gew&uuml;nschten Datum (<?php echo $dates['base']->format('d.m.Y'); ?>) gefunden werden!
+        <a href="<?php echo $this->config->base_url(); ?>"><br />Zur&uuml;ck</a></p>
+        
+        <ul class="pure-paginator date_nav">
+            <li><a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['before']->format('d/m/Y')); ?>">&#171; <?php echo $dates['before']->format('D d.m.Y'); ?></a></li>
+            <li><a class="pure-button pure-button-active" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['base']->format('d/m/Y')); ?>"><?php echo $dates['base']->format('D d.m.Y'); ?></a></li>
+            <li><a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['after']->format('d/m/Y')); ?>"><?php echo $dates['after']->format('D d.m.Y'); ?> &#187;</a></li>
+        </ul>
