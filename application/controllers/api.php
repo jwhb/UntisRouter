ea43e037842d6a6d81be37030503ed3187de654f
JWhy <jwhy@jwhy.de>
2014-01-10 22:00:07 +0100

    Implement first API approach
    Signed-off-by: JWhy <jwhy@jwhy.de>

---
+++ application/controllers/api.php
@@ -0,0 +1,29 @@
+<?php
+
+class Api extends MY_Controller{
+  
+  public function __construct(){
+    parent::__construct();
+  }
+  
+  public function index(){
+    exit('Erro: Invalid API call.');
+  }
+  
+  public function get($grade = 'all', $date = 'ahead'){
+    $chain = $substitutions = $this->substitutions->order_by('time');
+    $today = new DateTime('today');
+    $substs = array();
+    switch($date){
+    	case 'ahead':
+    	  $substs = $chain->get_many_by('date >= "' . $today->format('Y-m-d') . '"');
+    	  break;
+    	case 'anytime':
+    	  $substs = $chain->get_all();
+    	  break;
+    }
+    echo(json_encode($substs));
+    exit();
+  }
+  
+}
