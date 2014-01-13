8940ab672d08d36f8c0aa16080bf1761ea7c8934
JWhy <jwhy@jwhy.de>
2014-01-13 20:05:57 +0100

    Use localized day name in date menu views
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/views/substitutions/list.php
+++ application/views/substitutions/list.php
@@ -25,7 +25,16 @@
 
         
         <ul class="pure-paginator date_nav">
-            <li><a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['before']->format('d/m/Y')); ?>">&#171; <?php echo $dates['before']->format('D d.m.Y'); ?></a></li>
-            <li><a class="pure-button pure-button-active" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['base']->format('d/m/Y')); ?>"><?php echo $dates['base']->format('D d.m.Y'); ?></a></li>
-            <li><a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['after']->format('d/m/Y')); ?>"><?php echo $dates['after']->format('D d.m.Y'); ?> &#187;</a></li>
+            <li>
+              <a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['before']->format('d/m/Y')); ?>">&#171;
+              <?php echo strftime('%a, %d.%m.%y', $dates['before']->getTimestamp()); ?></a>
+            </li>
+            <li>
+              <a class="pure-button pure-button-active" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['base']->format('d/m/Y')); ?>">
+              <?php echo strftime('%a, %d.%m.%y', $dates['base']->getTimestamp()); ?></a>
+            </li>
+            <li>
+              <a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['after']->format('d/m/Y')); ?>">
+              <?php echo strftime('%a, %d.%m.%y', $dates['after']->getTimestamp()); ?> &#187;</a>
+            </li>
         </ul>
