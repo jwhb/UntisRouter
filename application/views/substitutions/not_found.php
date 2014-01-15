815a25367ee3bf70c87dbc5c17e0f1a4877be2e1
JWhy <jwhy@jwhy.de>
2014-01-15 21:17:35 +0100

    Extract date navigator into new view 'datenav'
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/views/substitutions/not_found.php
+++ application/views/substitutions/not_found.php
@@ -1,18 +1,3 @@
         <p style="text-align: center;">Für die gew&uuml;nschte Stufe (<?php echo $grade; ?>) konnten keine Eintr&auml;ge am gew&uuml;nschten Datum (<?php echo $dates['base']->format('d.m.Y'); ?>) gefunden werden!
         <a href="<?php echo $this->config->base_url(); ?>"><br />Zur&uuml;ck</a></p>
-        
-        
-        <ul class="pure-paginator date_nav">
-            <li>
-              <a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['before']->format('d/m/Y')); ?>">&#171;
-              <?php echo strftime('%a, %d.%m.%y', $dates['before']->getTimestamp()); ?></a>
-            </li>
-            <li>
-              <a class="pure-button pure-button-active" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['base']->format('d/m/Y')); ?>">
-              <?php echo strftime('%a, %d.%m.%y', $dates['base']->getTimestamp()); ?></a>
-            </li>
-            <li>
-              <a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['after']->format('d/m/Y')); ?>">
-              <?php echo strftime('%a, %d.%m.%y', $dates['after']->getTimestamp()); ?> &#187;</a>
-            </li>
-        </ul>
+        <br />
--- application/views/substitutions/single.php
+++ application/views/substitutions/single.php
@@ -23,18 +23,3 @@
             </tr><?php endforeach; ?> 
           </tbody>
         </table>
-        
-        <ul class="pure-paginator date_nav">
-            <li>
-              <a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['before']->format('d/m/Y')); ?>">&#171;
-              <?php echo strftime('%a, %d.%m.%y', $dates['before']->getTimestamp()); ?></a>
-            </li>
-            <li>
-              <a class="pure-button pure-button-active" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['base']->format('d/m/Y')); ?>">
-              <?php echo strftime('%a, %d.%m.%y', $dates['base']->getTimestamp()); ?></a>
-            </li>
-            <li>
-              <a class="pure-button" href="<?php echo $this->config->base_url('grades/' . $grade . '/' . $dates['after']->format('d/m/Y')); ?>">
-              <?php echo strftime('%a, %d.%m.%y', $dates['after']->getTimestamp()); ?> &#187;</a>
-            </li>
-        </ul>
