9b8e2ac805b94de1df0ace62bce30cdbdbdc7a5e
JWhy <jwhy@jwhy.de>
2014-01-17 21:45:05 +0100

    Modify API controller; allow custom dates
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -7,13 +7,14 @@ class Api extends MY_Controller{
   }
   
   public function index(){
-    exit('Error: Invalid API call.');
+    die('Error: Invalid API call.');
   }
   
   public function get($grade = 'all', $date = 'ahead'){
     $chain = $this->substitutions->order_by('date asc, grade asc, time');
     $today = new DateTime('today');
     $grade = mysql_real_escape_string($grade);
+    $date = mysql_real_escape_string($date);
     $grade_where = ($grade != 'all' && $grade != '')? "grade = '$grade' AND " : '';
     $substs = array();
     switch($date){
@@ -24,7 +25,11 @@ class Api extends MY_Controller{
     	  $substs = $chain->get_many_by($grade_where . 'date >= "' . $today->format('Y-m-d') . '"');
     	  break;
     	case 'anytime':
+    	  die('Date value not allowed. Use \'ahead\' instead!');
     	  $substs = $chain->get_many_by($grade_where);
+    	  break;
+    	default:
+    	  $substs = $chain->get_many_by($grade_where . 'date = "' . $date . '"');
     	  break;
     }
     echo(json_encode($substs));
