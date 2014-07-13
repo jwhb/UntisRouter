5a9d6fa3b0e48418e43ee09a3f1197fd0ff619a7
JWhy <jwhy@jwhy.de>
2014-07-14 01:52:18 +0200

    Refactor API controller to work RESTful
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -1,55 +1,36 @@
 <?php
+require_once (APPPATH . 'libraries/REST_Controller.php');
 
-class Api extends MY_Controller{
+class Api extends REST_Controller {
 
-  public function __construct(){
-    parent::__construct();
-    $this->load->model('substtext_model', 'substtext');
-  }
-
-  public function index(){
-    die('Error: Invalid API call.');
-  }
-
-  public function substitutions($grade = 'all', $date = 'ahead'){
-    $grade = mysql_real_escape_string($grade);
-    $date = mysql_real_escape_string($date);
-
-    $today = new DateTime('today');
-    $json = '';
-    $error = '';
-
-    if($grade == 'all' && $date == 'ahead'){
-      $this->load->helper('file');
-      $json = read_file('assets/export/vp_all_ahead.json');
-      if(!$json) $json = json_encode(array('error' => 'Could not access cached export file.', 'errno' => '501'));
-    }else{
-      $grade_where = ($grade != 'all' && $grade != '') ? "grade = '$grade'" : '1';
-
-      $date_where = '';
-      switch($date){
-        case 'today':
-          $date_where = 'date = "' . $today->format('Y-m-d') . '"';
-          break;
-        case 'ahead':
-          $date_where = 'date >= "' . $today->format('Y-m-d') . '"';
-          break;
-        case 'anytime':
-          die('Permissions denied. Use \'ahead\' as date value instead!');
-          $date_where = '1';
-          break;
-        default:
-          $date_where = 'date = "' . $date . '"';
-          break;
-      }
-
-      $substs = $this->substitutions->order_by('date asc, grade asc, time')->get_many_by("$date_where AND $grade_where");
-      $notes = $this->substtext->order_by('date')->get_many_by($date_where);
-      $json = json_encode(array('substitutions' => $substs, 'notes' => $notes));
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('substitution_model', 'substitutions');
+        $this->load->model('substtext_model', 'substtext');
     }
 
-    echo($json);
-    exit();
-  }
+    private function getGradeColor($grade) {
+        $grade = $grade{0};
+        $groups = ['5' => 1, '6' => 2, '7' => 3, '8' => 4, '9' => 5, 'E' => 6, 'Q' => 7];
+        return (isset($groups[$grade])) ? $groups[$grade] : 0;
+    }
 
+    function substitutions_get() {
+        $grade = $this->get('grade');
+        $date = ($this->get('date'))? $this->get('date') : 'ahead';
+        $sql_date = ($date != 'ahead')? $date : $this->substitutions->get_today_string();
+
+        $substitutions = $this->substitutions->get_grade($grade, $date);
+        $notes = $this->substtext->order_by('date')->get_many_by('date', $sql_date);
+        $this->response(array('substitutions' => $substitutions, 'notes' => $notes), 200);
+    }
+
+    function grades_get() {
+        $grades = $this->substitutions->get_uniques();
+        foreach ($grades as $id => $grade) {
+            $grades[$id] = array('id' => $id, 'name' => $grade, 'group' => $this->getGradeColor($grade));
+        }
+
+        $this->response($grades, 200);
+    }
 }
--- application/models/substitution_model.php
+++ application/models/substitution_model.php
@@ -1,30 +1,47 @@
 <?php
 
 class Substitution_model extends MY_Model {
-  
-  public $before_create = array('created_at','updated_at');
-  public $before_update = array('updated_at');
-  
-  public function get_uniques(){
-    $grades = array();
-    $table = $this->table();
-    $query = $this->db->query("SELECT DISTINCT grade FROM $table ORDER BY grade ASC");
-    foreach($query->result() as $row){
-      $grades[] = $row->grade;
-    }
-    return($grades);
-  }
-  
-  public function delete_empty(){
-    $this->delete_many_by_many(array('time' => '', 'teacher' => ''));
-  }
-  
-  public function get_all_ahead(){
-    $today = new DateTime('today');
-    $today = $today->format('Y-m-d');
-    
-    $data = $this->order_by('date asc, grade asc, time')->get_many_by("date >= '$today'");
-    return($data);
-  }
 
+    public $before_create = array('created_at', 'updated_at');
+
+    public $before_update = array('updated_at');
+
+    public function get_today_string() {
+        $date = new DateTime('today');
+        return $date->format('Y-m-d');
+    }
+
+    public function get_uniques() {
+        $grades = array();
+        $table = $this->table();
+        $query = $this->db->query("SELECT DISTINCT grade FROM $table ORDER BY grade ASC");
+        foreach ($query->result() as $row) {
+            $grades[] = $row->grade;
+        }
+        return ($grades);
+    }
+
+    public function delete_empty() {
+        $this->delete_many_by_many(array('time' => '', 'teacher' => ''));
+    }
+
+    public function get_all_ahead() {
+        $today = $this->get_today_string();
+        $data = $this->order_by('date asc, grade asc, time')->get_many_by("date >= '$today'");
+        return ($data);
+    }
+
+    public function get_grade($grade = 'all', $date = 'ahead') {
+        if ($date == 'ahead') {
+            $date = $this->get_today_string();
+            $where['date >='] = $date;
+        } elseif ($date != 'all') {
+            $where['date'] = $date;
+        }
+        if ($grade != 'all') {
+            $where['grade ='] = $grade;
+        }
+        $data = $this->order_by('date asc, grade asc, time')->get_many_by($where);
+        return ($data);
+    }
 }
