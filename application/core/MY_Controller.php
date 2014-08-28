dad88f619b2e498655189c1cfd7d05213dcd8575
JWhy <jwhy@jwhy.de>
2014-08-28 17:36:23 +0200

    Change controller constructor to make subst data loading optional
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,23 +1,25 @@
 <?php
 
 class MY_Controller extends CI_Controller {
-    
+
   protected function set_title($title = 'Page') {
     $this->template->write('title', $title);
   }
-  
-  public function __construct() {
+
+  public function __construct($load_subst_data = false) {
     parent::__construct();
-    $this->load->model('substitution_model', 'substitutions');
-    
+    if($load_subst_data){
+        $this->load->model('substitution_model', 'substitutions');
+        $data['grades'] = $this->substitutions->get_uniques();
+    }
+
     $data['menu'] = $this->config->item('page_menu');
     $data['controller'] = strtolower($this->uri->rsegment(1));
     if($this->uri->total_rsegments() > 1 && $data['controller'] == 'grades') $data['controller'] .= strtolower( $this->uri->slash_segment(2, 'leading'));
-    $data['grades'] = $this->substitutions->get_uniques();
-    
+
     $this->template->write_view('sidebar', 'sidebar', $data, TRUE);
   }
-  
+
   function index() {
     $this->template->render();
   }
