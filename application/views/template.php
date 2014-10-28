3434359248ccb56e8e3299867c192e0f4aa47173
JWhy <jwhy@jwhy.de>
2014-10-29 00:44:19 +0100

    Add favicon, substitute some HTML with CI helper functions
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/views/template.php
+++ application/views/template.php
@@ -1,13 +1,15 @@
-<!DOCTYPE html>
+<?php $this->load->helper('html'); 
+echo doctype('html5'); ?> 
 <html>
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php if(isset($grade) && !isset($substitutions)): echo $grade; ?> &middot; <?php endif; ?><?php echo $title; ?> &middot; <?php echo $this->config->item('app_name'); ?></title>
-  <link rel="stylesheet" href="<?php echo $this->config->base_url(); ?>assets/css/pure-min.css">
-  <link rel="stylesheet" href="<?php echo $this->config->base_url(); ?>assets/css/app.css">
-  <link rel="stylesheet" href="<?php echo $this->config->base_url(); ?>assets/css/font-awesome.min.css"><?php
-  echo $this->config->item('tracking', '') . "\n"; ?> 
+  <?php echo meta('viewport', 'width=device-width, initial-scale=1.0'); ?>
+  <title><?php if(isset($grade) && !isset($substitutions)) echo "$grade &middot; "; echo "$title &middot; " . $this->config->item('app_name'); ?></title>
+  <?php echo link_tag('assets/css/pure-min.css'); ?> 
+  <?php echo link_tag('assets/css/app.css'); ?> 
+  <?php echo link_tag('assets/css/font-awesome.min.css'); ?> 
+  <?php echo link_tag('assets/img/favicon.ico', 'icon', 'image/x-icon'); ?> 
+  <?php echo $this->config->item('tracking', ''); ?> 
 </head>
 <body>
   <div id="layout">
