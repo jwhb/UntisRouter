e8abe1539acefa58ec4c9ad4f3c681ab8ca2aa56
JWhy <jwhy@jwhy.de>
2014-10-29 21:43:47 +0100

    Randomize order in user list
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/views/profile/user_list.php
+++ application/views/profile/user_list.php
@@ -1,4 +1,4 @@
-        <div class="user_list"><?php foreach($users as $user): ?> 
+        <div class="user_list"><?php shuffle($users); foreach($users as $user): ?> 
             <?php echo anchor('profile/view/' . $user['username'], $user['first_name'] . ' ' . $user['last_name']); ?><br/>
         <?php endforeach; ?>
         </div>
