0acf1a50b23fca6ae10aff3f75e0659717e1879a
JWhy <jwhy@jwhy.de>
2015-05-08 01:31:07 +0200

    Add mod edit link to user list
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- application/views/profile/user_list.php
+++ application/views/profile/user_list.php
@@ -1,4 +1,5 @@
-        <div class="user_list"><?php shuffle($users); foreach($users as $user): ?> 
-            <?php echo anchor('profile/view/' . $user['username'], $user['first_name'] . ' ' . $user['last_name']); ?><br/>
+        <div class="user_list"><?php shuffle($users); foreach($users as $user): ?>
+            <?php echo anchor('profile/view/' . $user['username'], $user['first_name'] . ' ' . $user['last_name']); ?>
+            <?php if($is_mod) echo anchor('profile/edit/' . $user['username'], ' (edit)'); ?><br/>
         <?php endforeach; ?>
         </div>
